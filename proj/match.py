6bdf507a33b50a94b3e05d13bf2896d379440a27
Debian <admin@apex.sccwrp.org>
2022-10-04 18:03:27 +0000

    proposed fix for github issue #2

--- proj/match.py
+++ proj/match.py
@@ -131,7 +131,15 @@ def match(all_dfs):
     # I am not quite sure why we now do it so that the tables list matches the matched_tables list rather than the keys of the dictionary
     # But we will use this to get rid of the tox summary tab if they are submitting tox data with a summary
     # match_dataset = [k for k,v in datasets.items() if set(v.get('tables')) == set(all_dfs.keys())]
-    match_dataset = [k for k,v in datasets.items() if set(v.get('tables')) == set(matched_tables)]
+    # match_dataset = [k for k,v in datasets.items() if set(v.get('tables')) == set(matched_tables)]
+
+    # Once again, we are changing the way match_dataset gets defined
+    # The logic that previously defined match_dataset doesnt cover the case where a subset of submitted tables matches a dataset
+    # if a subset of submitted tables matches a dataset, but there is one or more submitted tabs that do not match
+    #  then core checks etc. will run, but the app will crash, since there is a tab that does not match any table
+    # We now will require a dataset to match all the tablenames in the match report. Note that the tablename will be empty if no table was matched for a certain tab
+    # In this case, 
+    match_dataset = [k for k,v in datasets.items() if set(v.get('tables')) == [x.get('tablename') for x in match_report]]
 
     
 
