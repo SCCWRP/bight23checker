a37ae537c73308f31c27efb4f6026b8415b3baa5
Debian <admin@apex.sccwrp.org>
2022-10-24 23:40:30 +0000

    seems like the field export download is working - main issue was the file was written to data_query but the dl link pointed at data_templates

--- proj/download.py
+++ proj/download.py
@@ -1,5 +1,5 @@
 import os, time
-from flask import send_file, Blueprint, jsonify, request, g, current_app
+from flask import send_file, Blueprint, jsonify, request, g, current_app, render_template, send_from_directory
 import pandas as pd
 from pandas import read_sql, DataFrame
 
@@ -12,13 +12,35 @@ def submission_file(submissionid, filename):
 
 @download.route('/export', methods = ['GET','POST'])
 def template_file():
-    #filename = request.args.get('filename')
-    #tablename = request.args.get('tablename')
+    filename = request.args.get('filename')
+    tablename = request.args.get('tablename')
     agency = request.args.get('agency')
     if request.args.get("agency"):
         agencycode = request.args.get("agency")
         if agencycode == "ABC":
             agency = "Aquatic Bioassay and Consulting Laboratories"
+        elif agencycode == "ANCHOR":
+            agency = "Anchor QEA"
+        elif agencycode == "AMEC":
+            agency = "AMEC, Foster, & Wheeler / WOOD"
+        elif agencycode == "CSD":
+            agency = "City of San Diego"
+        elif agencycode == "LACSD":
+            agency = "Los Angeles County Sanitation Districts"
+        elif agencycode == "MBC":
+            agency = "Marine Biological Consulting"
+        elif agencycode == "OCSD":
+            agency = "Orange County Sanitation Districts"
+        elif agencycode == "CLAEMD":
+            agency = "City of Los Angeles Environmental Monitoring Division"
+        elif agencycode == "CLAWPD":
+            agency = "City of Los Angeles Watershed Protection Division"
+        elif agencycode == "SCCWRP":
+            agency = "Southern California Coastal Water Research Project"
+        elif agencycode == "SPAWAR":
+            agency = "Space and Naval Warfare Systems Command"
+        elif agencycode == "WESTON":
+            agency = "Weston Solutions"
         print(f'agency: {agency}')
         # variables use timestamp at end of the export file IF storing and downloading export data file gives an issue
         #gettime = int(time.time())
@@ -27,8 +49,8 @@ def template_file():
         # add another folder within /export folder for returning data to user (either in browser or via excel file)
         # probably better to not store all these queries in an excel file for storage purposes - use timestamp if this is an issue
         # name after agency and table for now
-        export_file = f'/var/www/checker/export/data_query/{agencycode}-export.xlsx'
-        export_link = f'http://checker.sccwrp.org/checker/export/data_query/{agencycode}-export.xlsx'
+        export_file = os.path.join(os.getcwd(), "export", "data_query", f'{agencycode}-export.xlsx')
+        export_link = f'/export?filename={agencycode}-export.xlsx'
         export_writer = pd.ExcelWriter(export_file, engine='xlsxwriter')
         eng = g.eng
 
@@ -96,42 +118,110 @@ def template_file():
                                     mobile_occupation_grab
                                 WHERE samplingorganization = '{agency}';
                             """, eng)
-    print("working checkpoint")
-    print("occupation_df: ")
-    print(occupation_df)
+        print("working checkpoint")
+        print("occupation_df: ")
+        print(occupation_df)
+
+        # call to database to get trawl data
+        trawl_df = pd.read_sql(f""" SELECT 
+                                        trawlstationid as stationid,
+                                        date(trawloverdate) as sampledate,
+                                        trawlsamplingorganization as samplingorganization,
+                                        trawlgear as gear,
+                                        trawlnumber,
+                                        trawldatum as datum, 
+                                        (trawloverdate-interval'7 hours')::time as overtime, 
+                                        trawlovery as overlatitude, 
+                                        trawloverx as overlongitude,
+                                        (trawlstartdate-interval'7 hours')::time as starttime, 
+                                        trawlstarty as startlatitude, 
+                                        trawlstartx as startlongitude,
+                                        trawlstartdepth as startdepth, 
+                                        trawldepthunits as depthunits, 
+                                        trawlwireout as wireout,
+                                        (trawlenddate-interval'7 hours')::time as endtime, 
+                                        trawlendy as endlatitude, 
+                                        trawlendx as endlongitude,
+                                        trawlenddepth as enddepth, 
+                                        (trawldeckdate-interval'7 hours')::time as decktime, 
+                                        trawldecky as decklatitude, 
+                                        trawldeckx as decklongitude, 
+                                        trawlfail, 
+                                        ptsensor, 
+                                        ptsensormanufacturer, 
+                                        ptsensorserialnumber,
+                                        netonbottomtemp as onbottomtemp, 
+                                        netonbottomtime as onbottomtime, 
+                                        trawlcomments as comments
+                                    FROM mobile_trawl
+                                    WHERE
+                                        trawlsamplingorganization = '{agency}';
+                                """, eng)
+        print("trawl_df:")
+        print(trawl_df)
+        print(type(trawl_df))
+
+        # call to database to get grab data
+        grab_df = pd.read_sql(f"""SELECT
+                                    grabstationid as stationid,
+                                    date(grabdate) as sampledate, 
+                                    to_char((grabdate-interval'7 hours'), 'HH24:MI:SS') as sampletime,
+                                    grabnumber as grabeventnumber,
+                                    grabsamplingorganization as samplingorganization,
+                                    grabgear as gear, 
+                                    grabx as latitude,
+                                    graby as longitude, 
+                                    grabdatum as datum, 
+                                    grabstationwaterdepth as stationwaterdepth,
+                                    grabstationwaterdepthunits as stationwaterdepthunits, 
+                                    grabpenetration as penetration, 
+                                    grabpenetrationunits as penetrationunits, 
+                                    grabsedimentcomposition as composition, 
+                                    grabsedimentcolor as color, 
+                                    grabsedimentodor as odor, 
+                                    grabshellhash as shellhash, 
+                                    benthicinfauna, 
+                                    sedimentchemistry, 
+                                    grainsize, 
+                                    toxicity, 
+                                    grabfail, 
+                                    debris as debrisdetected, 
+                                    grabcomments as comments 
+                                FROM mobile_grab
+                                WHERE grabsamplingorganization = '{agency}';
+                                """, eng)
+        print("grab_df: ")
+        print(grab_df)
+        print(type(grab_df))
 
 
     if filename is not None:
-        return send_file( os.path.join(os.getcwd(), "export", "data_templates", filename), as_attachment = True, attachment_filename = filename ) \
-            if os.path.exists(os.path.join(os.getcwd(), "export", "data_templates", filename)) \
+        return send_file( os.path.join(os.getcwd(), "export", "data_query", filename), as_attachment = True, attachment_filename = filename ) \
+            if os.path.exists(os.path.join(os.getcwd(), "export", "data_query", filename)) \
             else jsonify(message = "file not found")
     
-    elif tablename is not None:
-        eng = g.eng
-        valid_tables = read_sql("SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'tbl%%';", g.eng).values
-        
-        if tablename not in valid_tables:
-            return "invalid table name provided in query string argument"
-        
-        data = read_sql(f"SELECT * FROM {tablename};", eng)
-        data.drop( set(data.columns).intersection(set(current_app.system_fields)), axis = 1, inplace = True )
+    with export_writer:
+        occupation_df.to_excel(export_writer, sheet_name = "occupation")
+        trawl_df.to_excel(export_writer, sheet_name = "trawl")
+        grab_df.to_excel(export_writer, sheet_name = "grab")
+    
 
-        datapath = os.path.join(os.getcwd(), "export", "data", f'{tablename}.csv')
+    return render_template('export.html', export_file=export_file, export_link=export_link, agency=agency)
 
-        data.to_csv(datapath, index = False)
+# Nick L - didn't quite get this working yet
+# idea is to serve export.html above, then have this route serve the exported file
+# @download.route('/export/data_query/<export_file>', methods = ['GET','POST'])
+# def data_query(export_file):
+#     return send_from_directory('export/data_query', export_file)
 
-        return send_file( datapath, as_attachment = True, attachment_filename = f'{tablename}.csv' )
-
-    else:
-        return jsonify(message = "neither a filename nor a database tablename were provided")
 
 # def template_file():
 #     filename = request.args.get('filename')
 #     tablename = request.args.get('tablename')
 
 #     if filename is not None:
-#         return send_file( os.path.join(os.getcwd(), "export", "data_templates", filename), as_attachment = True, attachment_filename = filename ) \
-#             if os.path.exists(os.path.join(os.getcwd(), "export", "data_templates", filename)) \
+#         return send_file( os.path.join(os.getcwd(), "export", "data_query", filename), as_attachment = True, attachment_filename = filename ) \
+#             if os.path.exists(os.path.join(os.getcwd(), "export", "data_query", filename)) \
 #             else jsonify(message = "file not found")
     
 #     elif tablename is not None:
