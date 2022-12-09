f35420bd0ec1fe91bc128448afed8dcd4085a1fd
Debian <admin@apex.sccwrp.org>
2022-12-09 00:20:44 +0000

    making sure a datatype is specified in report.py

--- proj/report.py
+++ proj/report.py
@@ -9,6 +9,8 @@ report_bp = Blueprint('report', __name__)
 def report():
     valid_datatypes = ['field', 'chemistry', 'infauna', 'toxicity']
     datatype = request.args.get('datatype')
+    if datatype is None:
+        return "No datatype specified"
     if datatype in valid_datatypes:
         report_df = pd.read_sql(f'select * from vw_{datatype}_completeness_report', g.eng)
         report_df.set_index(['submissionstatus', 'lab', 'parameter'], inplace = True)
@@ -16,4 +18,8 @@ def report():
         report_df = pd.DataFrame(columns = ['submissionstatus', 'lab', 'parameter', 'stations'])
         report_df.set_index(['submissionstatus', 'lab'], inplace = True)
 
-    return render_template('report.html',  tables=[report_df.to_html(classes=['w3-table','w3-bordered'], header="true", justify = 'left', sparsify = True)], report_title = f'{datatype.capitalize()} Completeness Report')
+    return render_template(
+        'report.html',  
+        tables=[report_df.to_html(classes=['w3-table','w3-bordered'], header="true", justify = 'left', sparsify = True)], 
+        report_title = f'{datatype.capitalize()} Completeness Report'
+    )
