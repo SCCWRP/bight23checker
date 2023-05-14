217b235a8cda65864a7df9bb0823d512196b1196
Debian <admin@apex.sccwrp.org>
2023-05-14 21:54:11 +0000

    added the schema exploration tool to the checker

--- proj/admin.py
+++ proj/admin.py
@@ -1,5 +1,8 @@
 import os
+import pandas as pd
 from flask import Blueprint, g, current_app, render_template, redirect, url_for, session, request, jsonify
+
+from .utils.db import metadata_summary
 
 admin = Blueprint('admin', __name__)
 
@@ -25,6 +28,46 @@ def tracking():
     
     # session is a reserved word in flask - renaming to something different
     return render_template('track.html', session_json=session_json, authorized=authorized)
+
+
+@admin.route('/schema')
+def schema():
+    authorized = session.get("AUTHORIZED_FOR_ADMIN_FUNCTIONS")
+
+    print("start schema information lookup routine")
+    eng = g.eng
+    datatype = request.args.get("datatype")
+    
+    if datatype is not None:
+        if datatype not in current_app.datasets.keys():
+            return f"Datatype {datatype} not found"
+
+        # dictionary to return
+        return_object = {}
+        
+        tables = current_app.datasets.get(datatype).get("tables")
+        for tbl in tables:
+            df = metadata_summary(tbl, eng)
+            
+            df['lookuplist_table_name'] = df['lookuplist_table_name'].apply(
+                lambda x: f"""<a target=_blank href=/{current_app.config.get('APP_SCRIPT_ROOT')}/scraper?action=help&layer={x}>{x}</a>""" if pd.notnull(x) else ''
+            )
+
+            # drop "table_name" column
+            df.drop('tablename', axis = 'columns', inplace = True)
+
+            # drop system fields
+            df.drop(df[df.column_name.isin(current_app.system_fields)].index, axis = 'rows', inplace = True)
+
+            df.fillna('', inplace = True)
+
+            return_object[tbl] = df.to_dict('records')
+        
+        return render_template('schema.html', metadata=return_object, datatype=datatype, authorized=authorized)
+        
+    # only executes if "datatypes" not given
+    datatypes_list = current_app.datasets.keys()
+    return render_template('schema.html', datatypes_list=datatypes_list, authorized=authorized)
 
 
 
