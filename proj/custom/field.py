af00cc93bbeaa566dc2b2328d5d76f975daf90cb
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2023-06-08 19:10:24 +0000

    changing the regular expression used to check the 24 hour clock time format

--- proj/custom/field.py
+++ proj/custom/field.py
@@ -167,9 +167,9 @@ def fieldchecks(occupation, eng, trawl = None, grab = None):
 
     print("# Check the time formats on all time columns")
     # Check the time formats on all time columns
-    def checkTime(df, col, args, time_format = re.compile(r'([0-9]{1,2}):[0-5][0-9]:[0-5][0-9]'), custom_errmsg = None):
+    def checkTime(df, col, args, time_format = re.compile(r'^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$'), custom_errmsg = None):
         """default to checking the 24 hour clock time"""
-    
+
         args.update({
             "badrows": df[~df[col.lower()].map(str).str.match(time_format)].tmp_row.tolist(),
             "badcolumn": col,
